0b8ad6b768b929f369375230f80d22eb4ab72f8d
SabinaWinehouse <sabika711@icloud.com>
2022-06-30 12:54:40 +0300

    corrections

--- scripts/script.js
+++ scripts/script.js
@@ -82,7 +82,7 @@ function callImagePopUp() {
       imagePopup.src = URL;
 
       console.log(imagePopup.src);
-      imagePopup.classList.toggle("popup");
+      imagePopup.classList.toggle("popup__open");
     });
   });
 }
