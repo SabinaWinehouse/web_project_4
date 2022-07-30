d9b02d26e5914f5e6a24a4b953540639a98a3ddb
SabinaWinehouse <sabika711@icloud.com>
2022-07-30 13:39:27 +0300

    validation corrections

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,17 +1,59 @@
+function showError(input, settings) {
+  const { inputErrorClass } = settings;
+  const error = input.validationMessage;
+  const errorElement = document.querySelector(`#${input.id}-error`);
+  errorElement.textContent = error;
+  input.classList.add(inputErrorClass);
+}
+// .popup__input_theme_error{
+//   color: Red;
+// }
+function hideError(input, settings) {
+  const { inputErrorClass } = settings;
+  const errorElement = document.querySelector(`#${input.id}-error`);
+  errorElement.textContent = "";
+  input.classList.remove(inputErrorClass);
+}
+
+function checkValidity(input, settings) {
+  if (input.validity.valid) {
+    hideError(input, settings);
+  } else {
+    showError(input, settings);
+  }
+}
+
+function toggleButtonState(inputs, button, settings) {
+  const { inactiveButtonClass } = settings;
+  const isFormValid = inputs.every((input) => input.validity.valid);
+  if (isFormValid) {
+    button.disaled = false;
+    button.classList.remove(inactiveButtonClass);
+  } else {
+    button.disaled = "disabled";
+    button.classList.add(inactiveButtonClass);
+  }
+  //add styles for popup__button_disabled class
+}
+
 function enableValidation(settings) {
-  const forms = [...document.querySelectorAll(".popup__form")];
+  const { formSelector, inputSelector, submitButtonSelector, ...rest } =
+    settings;
+  const forms = [...document.querySelectorAll(formSelector)];
+
   forms.forEach((form) => {
     form.addEventListener("submit", (e) => e.preventDefault());
-    const inputs = form.querySelectorAll(".popup__input");
-    console.log(inputs);
+
+    const inputs = [...form.querySelectorAll(inputSelector)];
+    const button = form.querySelector(submitButtonSelector);
+
+    inputs.forEach((input) => {
+      input.addEventListener("input", () => {
+        checkValidity(input, rest);
+        toggleButtonState(inputs, button, rest);
+      });
+    });
   });
-  //find all forms
-  //prevent their default behavior
-  //search for all inputs
-  //subscribe to its change
-  // check is input is valid
-  //if is valid do nothing
-  // if no show error === default browser message for input
 }
 
 const config = {
