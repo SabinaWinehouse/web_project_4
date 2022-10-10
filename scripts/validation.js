f6ca8c4db5ebece6238a92751c72e1f828cff4bb
SabinaWinehouse <sabika711@icloud.com>
2022-10-10 18:46:12 +0300

    very first corrections 7th

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,27 +1,41 @@
 const checkFormValidity = (inputs) =>
   inputs.every((input) => input.validity.valid);
-const config = {
+
+const validationConfig = {
   formSelector: ".popup__form",
+
   inputSelector: ".popup__input",
+
   submitButtonSelector: ".popup__button",
+
   inactiveButtonClass: "popup__button_disabled",
+
   inputErrorClass: "popup__input_type_error",
+
   errorClass: "popup__error_visible",
 };
-enableValidation(config);
+
+enableValidation(validationConfig);
 
 function showError(input, settings) {
   const { inputErrorClass } = settings;
+
   const error = input.validationMessage;
+
   const errorElement = document.querySelector(`#${input.id}-error`);
+
   errorElement.textContent = error;
+
   input.classList.add(inputErrorClass);
 }
 
 function hideError(input, settings) {
   const { inputErrorClass } = settings;
+
   const errorElement = document.querySelector(`#${input.id}-error`);
+
   errorElement.textContent = "";
+
   input.classList.remove(inputErrorClass);
 }
 
@@ -40,41 +54,34 @@ function toggleButtonState(inputs, button, settings) {
 
   if (isFormValid) {
     button.disabled = false;
+
     button.classList.remove(inactiveButtonClass);
   } else {
     button.disabled = true;
+
     button.classList.add(inactiveButtonClass);
   }
 }
-const setEventListeners = ( formSelector,{ inputSelector, submitButtonSelector, ...rest } =
-settings){
-  const inputs = [...form.querySelectorAll(inputSelector)];
-  const button = form.querySelector(submitButtonSelector);
-  
-  inputs.forEach((input) => {
-    input.addEventListener("input", () => {
-      toggleInputError(input, rest);
-      toggleButtonState(inputs, button, rest);
-    });
-  });
-}09;
-
-
 
 function enableValidation(settings) {
   const { formSelector, inputSelector, submitButtonSelector, ...rest } =
     settings;
+
   const forms = [...document.querySelectorAll(formSelector)];
 
   forms.forEach((form) => {
     form.addEventListener("submit", (e) => e.preventDefault());
 
     const inputs = [...form.querySelectorAll(inputSelector)];
+
     const button = form.querySelector(submitButtonSelector);
+
     toggleButtonState(inputs, button, settings);
+
     inputs.forEach((input) => {
       input.addEventListener("input", () => {
         toggleInputError(input, rest);
+
         toggleButtonState(inputs, button, rest);
       });
     });
