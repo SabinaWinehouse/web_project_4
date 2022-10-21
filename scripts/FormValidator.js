fed75eda2963d347fc4dcac57d5ae8bbb8eecfc5
SabinaWinehouse <sabika711@icloud.com>
2022-10-21 09:21:42 +0300

    formValidator

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -1,43 +1,88 @@
 class FormValidator {
-  constructor(config) {}
+  constructor(settings, formElement) {
+    this.settings = settings;
+    this.formElement = formElement;
+  }
+
+  _showError(input, error) {
+    const { inputErrorClass, errorClass } = this.settings;
+    const error = input.validationMessage;
+    const errorElement = this.formElement.querySelector(`#${input.id}-error`);
+    errorElement.textContent = error;
+    input.classList.add(inputErrorClass);
+    errorElement.classList.add(errorClass);
+  }
+
+  _hideError(input) {
+    const { inputErrorClass, errorClass } = this.settings;
+    const errorElement = this.formElement.querySelector(`#${input.id}-error`);
+    errorElement.textContent = "";
+    input.classList.remove(inputErrorClass);
+    errorElement.classList.remove(errorClass);
+  }
+
+  _checkFormValidity = (input) => {
+    if (!input.validity.valid) {
+      this._showError(input, input.validationMessage);
+    } else {
+      this._hideError(input);
+    }
+  };
+
+  _setEventListeners = () => {
+    const { inputSelector } = this.settings;
+
+    this.inputs = [...this.formElement.querySelectorAll(inputSelector)];
+
+    this.inputs.forEach((inputElement) => {
+      inputElement.addEventListener("input", () => {
+        this._checkFormValidity(inputElement, rest);
+        toggleInputError();
+
+        toggleButtonState();
+      });
+    });
+  };
+
+  _toggleInputError(input) {
+    if (input.validity.valid) {
+      hideError(input, settings);
+    } else {
+      showError(input, settings);
+    }
+  }
+
+  _toggleButtonState() {
+    const { inactiveButtonClass } = this.settings;
+    const isFormValid = _checkFormValidity();
+    if (isFormValid) {
+      button.disabled = false;
+      button.classList.remove(inactiveButtonClass);
+    } else {
+      button.disabled = true;
+      button.classList.add(inactiveButtonClass);
+    }
+  }
+
+  enableValidation() {
+    this.formElement.addEventListener("submit", (e) => {
+      e.preventDefault();
+    });
+    this._setEventListeners();
+  }
 }
-const editFormValidator = new FormValidator(validationConfig, editProfileForm);
-const addFormValidator = new FormValidator(validationConfig, createCardForm);
+const settings = {
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible",
+};
 
-// class FormValidator {
-//   constructor(settings, formElement) {
-//     this.settings = settings;
-//     this.formElement = formElement;
-//   }
+const editForm = document.querySelector(".popup__form");
+const addCardForm = document.querySelector(".popup__form");
 
-//   enableValidation() {
-//     const { formSelector, inputSelector, submitButtonSelector, ...rest } =
-//       settings;
-
-//     this.formElement.addEventListener("submit", (e) => e.preventDefault());
-
-//     const inputs = [...form.querySelectorAll(inputSelector)];
-//     const button = form.querySelector(submitButtonSelector);
-//     toggleButtonState(inputs, button, settings);
-//     inputs.forEach((input) => {
-//       input.addEventListener("input", () => {
-//         toggleInputError(input, rest);
-//         toggleButtonState(inputs, button, rest);
-//       });
-//     });
-//   }
-// }
-// const settings = {
-//   inputSelector: ".popup__input",
-//   submitButtonSelector: ".popup__button",
-//   inactiveButtonClass: "popup__button_disabled",
-//   inputErrorClass: "popup__input_type_error",
-//   errorClass: "popup__error_visible",
-// };
-
-// const editForm = document.querySelector(".popup__form");
-// const addCardForm = document.querySelector(".popup__form");
-
-// const editFormValidator = new FormValidator(settings, editForm);
-// const addFormValidator = new FormValidator(settings, addCardForm);
-// formValidator.enableValidation();
+const editFormValidator = new FormValidator(settings, editForm);
+const addFormValidator = new FormValidator(settings, addCardForm);
+FormValidator.enableValidation();
+FormValidator._showError();
